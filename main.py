055996a91d812ec94db5438f551de73a42461e15
madvirus-ops <edwinayabie1@gmail.com>
2023-03-17 22:15:51 +0100

    requirements

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from user import router as user_router
 app = FastAPI()
 app.include_router(user_router.router)
 
-@app.get("/")
-async def root():
-    return{"introduction": "ntoju backend server"}
+
+#using this to test the oauth
+
+from fastapi import  Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="templates")
+
+
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("signin.html", {"request": request})
