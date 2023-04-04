1bac6f9d024e6a4bc374cfbc20662ca7f5ad807e
seyiadel <seyiadel03@gmail.com>
2023-04-04 11:51:24 +0100

    Fix: products.name length(stringTruncation)

--- products/models.py
+++ products/models.py
@@ -9,7 +9,7 @@ class Products(Base):
     __tablename__ = "products"
     pkid = Column(Integer,primary_key=True)
     id = Column(UUID(as_uuid=True), default=uuid.uuid4,unique=True)
-    name = Column(String(10))
+    name = Column(String(length=150))
     description = Column(String)
     price = Column(Integer)
     created_at = Column(DateTime, default=datetime.utcnow)
