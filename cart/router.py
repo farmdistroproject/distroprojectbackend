5d0548ae6105d775652b90b4fc8e5d2f0e2fa5c8
seyiadel <seyiadel03@gmail.com>
2023-04-10 21:33:11 +0100

    Cart is_paid field to True if payment went through

--- cart/router.py
+++ cart/router.py
@@ -71,12 +71,13 @@ async def checkout(checkout_id: str,user: dict = Depends(get_current_user),db: S
 
     total_price = checkout_obj.amount
     if user.wallet_balance < total_price:
-        return {"response":"Money not enough, Please add money"}
+        return {"response":"Money not enough, Please add money", "status": False}
     else:
         user.wallet_balance -= total_price
+        checkout_obj.is_paid == True
     db.commit()
 
-    return {"response":"Baller Checked Out!","user_wallet_balance": user.wallet_balance, "cart": cart, "price": total_price}
+    return {"response":"Baller Checked Out!", "status": True, "user_wallet_balance": user.wallet_balance, "price": total_price}
 
 
 @router.get("/user/cart-items")
